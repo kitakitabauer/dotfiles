0d4254efea3b1cc7521028e0436c12181e19b788
kitakitabauer <kitakita.d.kitakita@gmail.com>
2016-09-07 22:52:21 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dotfiles3
+# dotfiles
+## Installation
+
+```bash
+# clone me
+$ cd ~
+$ git clone git@github.com:kitakitabuer/dotfiles3.git
+$ cd dotfiles
+
+# install dotfiles
+$ sh symlink.sh
+```
---
+++ symlink.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+basepath=$(cd $(dirname $0);pwd)
+
+# symlink dotfiles into ~
+files=.*
+for file in $files
+do
+    if [ $file != "." -a $file != ".." -a $file != ".git" ] ; then
+        ln -sf $basepath/$file ~
+    fi
+done
+
+# symlink binaries into /usr/local/bin
+files=bin/*
+for file in files
+do
+    ln -sf $basepath/$file /usr/local/bin
+done
